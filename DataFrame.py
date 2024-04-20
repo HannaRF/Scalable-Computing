bae8bddc1abcb37944b313ac1526c0a91c7e68bf
George Vaz <georgebada@hotmail.com>
2024-04-20 19:02:19 -0300

    Adicione comentários e exemplo de __getitem__

--- DataFrame.py
+++ DataFrame.py
@@ -5,7 +5,8 @@ class DataFrame:
         """Classe start 
 
         Args:
-            data (dict): Dictionary with keys as the columns names and values as columns. Defaults to None.
+            data (dict): Dictionary with keys as the columns names and values as columns. 
+                         Defaults to None.
         """
         self.__data = data
 
@@ -26,6 +27,11 @@ class DataFrame:
         return self.__valores
     
     def add_row(self, row):
+        """Add row to the DataFrame.
+
+        Args:
+            row (list or numpy.ndarray): List or numpy array with row values
+        """
         self.__valores = np.vstack((self.__valores, np.array(row, dtype=object)))
 
     def add_column(self, column):
@@ -46,9 +52,25 @@ class DataFrame:
         self.__valores = np.hstack((self.__valores, new_cols))
 
     def get_row(self, index):
+        """Get row by index
+
+        Args:
+            index (int): Integer representing the index 
+
+        Returns:
+            numpy.ndarray: Numpy array with the searched row
+        """
         return self.__valores[index]
 
     def get_column(self, name):
+        """Get column by column name
+
+        Args:
+            name (str): String with column name
+
+        Returns:
+            numpy.ndarray: Numpy array with the searched column 
+        """
         index = np.where(self.columns == name)[0][0]
         return self[:,index].copy().astype(type(self[0,index]))
 
@@ -69,24 +91,28 @@ class DataFrame:
         """
         return self.values[item]
 
-# Exemplo de uso:
-data = {
-    "Name": ["Alice", "Bob", "Charlie"],
-    "Age": [30, 35, 25],
-    "Occupation": ["Engineer", "Developer", "Designer"]
-}
+# # Exemplo de uso:
+# data = {
+#     "Name": ["Alice", "Bob", "Charlie"],
+#     "Age": [30, 35, 25],
+#     "Occupation": ["Engineer", "Developer", "Designer"]
+# }
 
-df = DataFrame(data)
-print(df)
+# df = DataFrame(data)
+# print(df)
 
-# Adicionar uma nova linha
-df.add_row(["David", 40, "Manager "])
-#print(df)
+# # Adicionar uma nova linha
+# df.add_row(["David", 40, "Manager "])
+# #print(df)
 
-# # Adicionar uma nova coluna
-df.add_column({"Location": ["New York", "San Francisco", "Los Angeles", "Chicago"]})
-print(df)
+# # # Adicionar uma nova coluna
+# df.add_column({"Location": ["New York", "San Francisco", "Los Angeles", "Chicago"]})
+# print(df)
 
-# # Acessar linha e coluna
-print(df.get_row(1))
-print(df.get_column("Name"))
+# # # Acessar linha e coluna
+# print(df.get_row(1))
+# print(df.get_column("Name"))
+
+# print(df[1,:])
+# print(df[:,1])
+# print(df[1,1])
