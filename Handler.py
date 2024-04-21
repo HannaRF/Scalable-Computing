eafb307d3b36b97e45529edbff370cd790ed3f67
yonirg <yonirabinovicigherman@gmail.com>
2024-04-21 19:12:38 -0300

    Adicione o tratador de soma de rendimentos

--- Handler.py
+++ Handler.py
@@ -1,4 +1,4 @@
-from queue import Queue  # Mudei de 'Queue' para 'queue'
+from Queue import Queue  # Mudei de 'Queue' para 'queue'
 from DataFrame import DataFrame  # Mantive a importação da classe DataFrame
 
 class Handler:
@@ -9,10 +9,10 @@ class Handler:
     def handle(self):
         for i, queue in enumerate(self.inputQueues):
             output_queue = Queue()
-            while not queue.empty():  # Mudei de 'is_empty' para 'empty'
-                dataframe = queue.get()  # Mudei de 'dequeue' para 'get'
+            while not queue.is_empty():  # Mudei de 'is_empty' para 'empty'
+                dataframe = queue.dequeue()  # Mudei de 'dequeue' para 'get'
                 processeddataframe = self.process(dataframe)
-                output_queue.put(processeddataframe)  # Mudei de 'enqueue' para 'put'
+                output_queue.enqueue(processeddataframe)  # Mudei de 'enqueue' para 'put'
             self.outputQueues[i] = output_queue
 
     def process(self, dataframe: DataFrame):
@@ -68,48 +68,123 @@ class TratadorFiltroNomeCodigo(Handler):
         filtered_rows = [row for row in dataframe.values if row[col_index] == self.termo_pesquisa]  # Mudei de 'dataframe._data' para 'dataframe.values'
         return DataFrame(dict(zip(dataframe.columns, zip(*filtered_rows))))
 
-class TratadorMerge(Handler):
-    def __init__(self, input_queues: list, chave_merge: str):
-        super().__init__(input_queues)
-        self.chave_merge = chave_merge
+# class TratadorMerge(Handler):
+#     def __init__(self, input_queues: list, chave_merge: str):
+#         super().__init__(input_queues)
+#         self.chave_merge = chave_merge
 
-    def process(self, dataframe: DataFrame) -> DataFrame:
-        merged_data = {}
+#     def process(self, dataframe: DataFrame) -> DataFrame:
+#         merged_data = {}
         
-        for i, queue in enumerate(self.inputQueues):
-            while not queue.empty():
-                df = queue.get()
-                for row in df.values:
-                    if isinstance(row, dict):
-                        chave = row.get(self.chave_merge)
-                    else:
-                        chave = row[list(dataframe.columns).index(self.chave_merge)]  # Mudei de 'dataframe._columns' para 'dataframe.columns'
-                    if chave in merged_data:
-                        merged_data[chave].append(row)
-                    else:
-                        merged_data[chave] = [row]
+#         for i, queue in enumerate(self.inputQueues):
+#             while not queue.is_empty():
+#                 df = queue.dequeue()
+#                 for row in df.values:
+#                     if isinstance(row, dict):
+#                         chave = row.get(self.chave_merge)
+#                     else:
+#                         chave = row[list(dataframe.columns).index(self.chave_merge)]  # Mudei de 'dataframe._columns' para 'dataframe.columns'
+#                     if chave in merged_data:
+#                         merged_data[chave].append(row)
+#                     else:
+#                         merged_data[chave] = [row]
 
-        merged_dfs = []
+#         merged_dfs = []
         
-        for chave, linhas in merged_data.items():
-            merged_df = DataFrame(dict(zip(dataframe.columns, zip(*linhas))))
-            merged_dfs.append(merged_df)
+#         for chave, linhas in merged_data.items():
+#             merged_df = DataFrame(dict(zip(dataframe.columns, zip(*linhas))))
+#             merged_dfs.append(merged_df)
         
-        return merged_dfs
+#         return merged_dfs
 
 #Tratador que soma os valores de uma coluna específica
-class TratadorSoma(Handler):
-    def __init__(self, input_queues: list, coluna_soma: str):
-        super().__init__(input_queues)
-        self.coluna_soma = coluna_soma
+class TratadorSomaValoresPorFila(Handler):
+    def __init__(self, inputQueues, coluna):
+        super().__init__(inputQueues)
+        self.coluna = coluna
 
-    def process(self, dataframe: DataFrame) -> DataFrame:
-        col_index = list(dataframe.columns).index(self.coluna_soma)  # Mudei de 'dataframe._columns' para 'dataframe.columns'
-        soma = sum([row[col_index] for row in dataframe.values])  # Mudei de 'dataframe._data' para 'dataframe.values'
-        return soma
+    def handle(self):
+        output_queues = []
+        for input_queue in self.inputQueues:
+            total_sum = 0
+            while not input_queue.is_empty():
+                df = input_queue.dequeue()
+                col_values = df.get_column(self.coluna)  # Obtendo os valores da coluna especificada
+                total_sum += sum(col_values)  # Somando os valores da coluna especificada
+            output_queue = Queue()
+            output_queue.enqueue(DataFrame({"Queue_Total_Sum": [total_sum]}))
+            output_queues.append(output_queue)
+        return output_queues
+
+
+
+class TratadorSomaTotalEntreFilas(Handler):
+    def __init__(self, inputQueues):
+        super().__init__(inputQueues)
+
+    def handle(self):
+        total_sum = 0
+        for fila in self.inputQueues:
+            fila_soma = 0
+            while not fila.is_empty():
+                df = fila.dequeue()
+                fila_soma += df.values[0][0]  # Somando os valores de Total_sum de cada DataFrame na fila
+            total_sum += fila_soma
+        
+        output_queues = []
+        for i, fila in enumerate(self.inputQueues):
+            output_queue = Queue()
+            if i == 0:
+                output_queue.enqueue(DataFrame({"Total_Total_sum": [total_sum]}))
+            else:
+                output_queue.enqueue(None)
+            output_queues.append(output_queue)
+        
+        return output_queues
+
+
+
+
+class TratadorMergeDataFrames(Handler):
+    def __init__(self, inputQueues, on, how='inner'):
+        super().__init__(inputQueues)
+        self.on = on
+        self.how = how
+
+    def handle(self):
+        # Unifica todos os DataFrames de cada fila em um único DataFrame maior
+        merged_dataframes = [self._merge_dataframes(queue) for queue in self.inputQueues if not queue.is_empty()]
+
+        # Verifica se há DataFrames para mesclar
+        if not merged_dataframes:
+            return []
+
+        # Faz o merge entre todos os DataFrames
+        final_merged_df = merged_dataframes[0]
+        for df in merged_dataframes[1:]:
+            final_merged_df = self._merge_dataframes(final_merged_df, df, self.on, self.how)
+
+        # Retorna o DataFrame resultante do merge
+        return [final_merged_df]
+
+    def _merge_dataframes(self, queue):
+        merged_data = {}
+        while not queue.is_empty():
+            df = queue.dequeue()
+            for col in df.columns:
+                if col not in merged_data:
+                    merged_data[col] = []
+                merged_data[col].extend(df.get_column(col))
+        if not merged_data:
+            return DataFrame({})
+        return DataFrame(merged_data)
+
+
+
 
 
 # Exemplo de uso:
+
 
 # Criar filas de entrada
 input_queue = Queue()
@@ -218,41 +293,73 @@ df4 = DataFrame({
 print("DataFrame original:")
 print(df3)
 # Adicionar DataFrame à fila de entrada
-input_queue.put(df)
-input_queue.put(df2)
-input_queue2.put(df3)
-input_queue2.put(df4)
+input_queue.enqueue(df)
+input_queue.enqueue(df2)
+input_queue2.enqueue(df3)
+input_queue2.enqueue(df4)
 
 # Instanciar e usar tratador de limpeza CSV
 tratador_limpeza_csv = TratadorLimpezaCSV(input_queues)
 tratador_limpeza_csv.handle()
-df_limpo = tratador_limpeza_csv.outputQueues[0].get()  # Mudei de 'peek' para 'get'
-df_limpo2 = tratador_limpeza_csv.outputQueues[1].get()  # Mudei de 'peek' para 'get'
+df_limpo = tratador_limpeza_csv.outputQueues[0].dequeue()  # Mudei de 'peek' para 'get'
+df_limpo2 = tratador_limpeza_csv.outputQueues[1].dequeue()  # Mudei de 'peek' para 'get'
 print("DataFrame limpo:")
 print(df_limpo2)
 
 # Instanciar e usar tratador de filtro por nome e código
 tratador_filtro_nome_codigo = TratadorFiltroNomeCodigo(tratador_limpeza_csv.outputQueues, 3, "Código Artista")
 tratador_filtro_nome_codigo.handle()
-df_filtrado = tratador_filtro_nome_codigo.outputQueues[0].get() # Mudei de 'peek' para 'get'
+df_filtrado = tratador_filtro_nome_codigo.outputQueues[0].dequeue() # Mudei de 'peek' para 'get'
 # Imprimir DataFrame filtrado
 print("DataFrame filtrado:")
 print(df_filtrado)
 
 # Adicionar DataFrame à fila de entrada novamente
-input_queue.put(df)
-input_queue.put(df2)
-input_queue2.put(df3)
-input_queue2.put(df4)
+input_queue.enqueue(df)
+input_queue.enqueue(df2)
+input_queue2.enqueue(df3)
+input_queue2.enqueue(df4)
 input_queues = [input_queue, input_queue2]
 
-# Instanciar e usar tratador de merge
-tratador_merge = TratadorMerge(input_queues, "Código Artista")
-tratador_merge.handle()
-df_mesclado = tratador_merge.outputQueues[0].get()[0]
+# # Instanciar e usar tratador de merge
+# tratador_merge = TratadorMerge(input_queues, "Código Artista")
+# tratador_merge.handle()
+# df_mesclado = tratador_merge.outputQueues[0].dequeue()[0]
+# # Imprimir DataFrame mesclado
+# print("DataFrame mesclado:")
+# print(df_mesclado)
+
+# print(df2)
+
+# Instanciar e usar tratador de soma de valores
+
+input_queue = Queue()
+input_queue2 = Queue()
+input_queue.enqueue(df)
+input_queue.enqueue(df2)
+input_queue2.enqueue(df3)
+input_queue2.enqueue(df4)
+
+input_queues = [input_queue, input_queue2]
+tratador_soma_valores = TratadorSomaValoresPorFila(input_queues, "Valor")
+soma_total = tratador_soma_valores.handle()
+
+# Imprimir soma total
+print("Soma por fila:")
+print(soma_total[0].peek())
+
+# Instanciar e usar tratador de soma entre filas
+tratador_soma_valores_entre_filas = TratadorSomaTotalEntreFilas(soma_total)
+soma_total_entre_filas = tratador_soma_valores_entre_filas.handle()
+# Imprimir soma total entre filas
+print("Soma total entre as filas:")
+print(soma_total_entre_filas[0].dequeue())
+
+# Instanciar e usar tratador de merge de DataFrames
+tratador_merge_dataframes = TratadorMergeDataFrames(input_queues, on="Código Artista")
+df_merged = tratador_merge_dataframes.handle()[0]
 # Imprimir DataFrame mesclado
 print("DataFrame mesclado:")
-print(df_mesclado)
+print(df_merged)
 
-print(df2)
 
