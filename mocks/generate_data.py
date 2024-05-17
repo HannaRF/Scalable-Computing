6645cbe18840b50db5a9f2a964d6d2b04b6a49df
Rian Freitas da Silva <85463854+Rian-Freitas@users.noreply.github.com>
2024-05-17 15:59:07 -0300

    Final mocks for CadeAnalytics

--- mocks/generate_data.py
+++ mocks/generate_data.py
@@ -1,19 +1,17 @@
 from faker import Faker
 import random
-import requests
 import pandas as pd
+import numpy as np
 import time
 
 # This class is used to generate fake data for testing purposes
-class generate_data:
-    def __init__(self, num_cycles=200, secs_between_cycles=10):
+class ContaVerdeMock:
+    def __init__(self):
         self.fake = Faker("pt_BR")
         self.number_of_users = 0
         self.number_of_products = 0
         self.number_of_storage_items = 0
         self.number_of_orders = 0
-        self.num_cycles = num_cycles
-        self.secs_between_cycles = secs_between_cycles
 
     def generate_user(self) -> dict:
         self.number_of_users += 1
@@ -97,21 +95,61 @@ class generate_data:
         
         pd.DataFrame(orders).to_csv("mocks/data/orders.csv", index=False)
 
+
+class CadeAnalyticsMock(ContaVerdeMock):
+
+    def __init__(self, num_users: int, num_products: int):
+        self.events = ["CLICK", "SCROLL", "ZOOM", "DBCLICK"]
+        self.fake = Faker()
+        self.number_of_users = num_users
+        self.number_of_products = num_products
+    
+    def get_data(self, num_requests):
+        data = []
+        
+        for _ in range(num_requests):
+            event_data = {
+                "data_notificacao": self.fake.date_time_this_month().strftime("%Y-%m-%d %H:%M:%S"),
+                "id_usuario": random.randint(1, self.number_of_users),
+                "estimulo": random.choice(self.events),
+                "componente_alvo": self.generate_componente_alvo()
+            }
+            data.append(event_data)
+        
+        return data
+    
+    def generate_componente_alvo(self):
+        elementos = ["BOTAO", "IMAGEM", "TEXTO", "INPUT", "SELECT"]
+        componentes = ["HOME", f"PRODUTO {random.randint(1, self.number_of_products)}", "CARRINHO", "PERFIL", "PAGAMENTO"]
+        return f"{random.choice(elementos)} - {np.random.choice(componentes, p=[0.1, 0.65, 0.1, 0.05, 0.1])}"
+
+class generateData:
+    def __init__(self, num_cycles: int, secs_between_cycles: int):
+        self.num_cycles = num_cycles
+        self.secs_between_cycles = secs_between_cycles
+    
     def run(self):
         cycles = 0
         while cycles < self.num_cycles:
-            self.generate_users_file(random.randint(1, 20))
-            self.generate_products_file(random.randint(1, 100))
-            self.generate_storage_file()
-            self.generate_orders_file(random.randint(1, 200))
+            self.conta_verde = ContaVerdeMock()
+            self.conta_verde.generate_users_file(random.randint(1, 100))
+            self.conta_verde.generate_products_file(random.randint(1, 100))
+            self.conta_verde.generate_storage_file()
+            self.conta_verde.generate_orders_file(random.randint(1, 100))
+
+            self.cade_analytics = CadeAnalyticsMock(self.conta_verde.number_of_users, self.conta_verde.number_of_products)
+            num_requests = random.randint(1, 200)
+            data = self.cade_analytics.get_data(num_requests)
+
+            print(data)
 
             self.number_of_orders = 0
             self.number_of_products = 0
             self.number_of_storage_items = 0
             self.number_of_users = 0
 
-            cycles += 1
             time.sleep(self.secs_between_cycles)
+            cycles += 1
 
 if __name__ == "__main__":
-    generate_data(num_cycles=100, secs_between_cycles=5).run()
+    generateData(10, 10).run()
